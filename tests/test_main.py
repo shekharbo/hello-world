d2d4447283bef18e98e4e050de587841a1a1a3d1
shekharbo <shekharbo@cybage.com>
2024-06-19 06:11:26 +0000

    added src and tests dir

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,7 +1,10 @@
+import sys
+sys.path.append('/home/ubuntu/workspace/hello_world_demo')
 
 
 import pytest
-from hello_world.main import main
+#from hello_world.main import main
+from hello_world import main
 
 def test_main(capsys):
     main()
